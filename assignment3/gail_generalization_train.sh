bd0d2fada9c0a695c4a7e7baa3dc2003a9fa5ea3
FelixLQJiang <90719360+FelixLQJiang@users.noreply.github.com>
2022-11-20 16:54:45 -0800

    ...

--- assignment3/gail_generalization_train.sh
+++ assignment3/gail_generalization_train.sh
@@ -1,6 +1,16 @@
 # Example script to run a batch of PPO generalization experiments
 
-for num in 1 5 10 20 50 100; do
+# for num in 1 5 10 20 50 100; do
+#   python train_gail.py \
+#   --env-id MetaDrive-Tut-${num}Env-v0 \
+#   --log-dir MetaDrive-Tut-${num}Env-v0 \
+#   --num-envs 10 \
+#   --max-steps 1000000 \
+#   --expert-dataset-size 30000 \
+#   > gail_metadrive_${num}env_train.log 2>&1 &
+# done
+
+for num in 1; do
   python train_gail.py \
   --env-id MetaDrive-Tut-${num}Env-v0 \
   --log-dir MetaDrive-Tut-${num}Env-v0 \
--- assignment3/td3_generalization_train.sh
+++ assignment3/td3_generalization_train.sh
@@ -1,5 +1,12 @@
 # Example script to run a batch of PPO generalization experiments
-for num in 1 5 10 20 50 100; do
+# for num in 1 5 10 20 50 100; do
+#   python train_td3.py \
+#   --env-id MetaDrive-Tut-${num}Env-v0 \
+#   --log-dir MetaDrive-Tut-${num}Env-v0 \
+#   --max-steps 500000 > td3_metadrive_${num}env.log 2>&1 &
+# done
+
+for num in 1; do
   python train_td3.py \
   --env-id MetaDrive-Tut-${num}Env-v0 \
   --log-dir MetaDrive-Tut-${num}Env-v0 \
